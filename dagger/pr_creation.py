826816ba58f04bba4a6736e961e28189992307c8
pgoslatara <pgoslatara@gmail.com>
2023-01-20 13:34:12 +0100

    Adding 3.10 to matrix

--- dagger/pr_creation.py
+++ dagger/pr_creation.py
@@ -4,7 +4,7 @@ import dagger
 
 
 async def test():
-    versions = ["3.9"] #, "3.10"]
+    versions = ["3.9", "3.10"]
 
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         src = client.host().directory(".")
@@ -15,11 +15,8 @@ async def test():
                     client.container().from_(f"python:{version}-slim-buster")
                     .with_mounted_directory("/src", src)
                     .with_workdir("/src")
-                    .with_env_variable("DBT_PROFILE_PATH", ".")
                     .with_exec(["apt", "update"])
                     .with_exec(["apt", "install", "git", "-y"])
-                    # .with_exec(["export", "DBT_PROFILE_PATH='.'"])
-                    .with_exec(["echo", "$DBT_PROFILE_PATH"])
                     .with_exec(["pip", "install", "--upgrade", "pip"])
                     .with_exec(["pip", "install", "-r", "requirements.txt"])
                     .with_exec(["dbt", "debug", "--profiles-dir", "/src"])
